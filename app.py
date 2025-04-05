056257c34ba8a4a3e93361b58798efc76d630be0
Andrew Tsai <andrewtsai0411@gmail.com>
2025-04-05 21:43:40 +0800

    Add files via upload
    - 將proof_of_concept.py中方法改寫為類別，使物件可被重複使用
    - 將app.py中互動元件由使用者自行輸入的文字框改為下拉式選單，並利用事件監聽器偵測，使選單能動態改變
    - 調整taiwan_presidential_election_2024.pbix文字呈現

--- app.py
+++ app.py
@@ -1,12 +1,76 @@
 import gradio as gr
-from proof_of_concept import calculate_cosine_similarity, filter_county_town_village
+from proof_of_concept import election
 
-country_percentage, cosine_similarity_df = calculate_cosine_similarity()
+country_percentage, cosine_similarity_df = election.calculate_cosine_similarity()
 ko_wu, lai_hsiao, hou_chao = country_percentage
 
-interface= gr.Interface(
-        fn=filter_county_town_village,
-        inputs=[gr.DataFrame(cosine_similarity_df),'text', 'text', 'text'],
-        outputs='dataframe', title='找出章魚里',
-        description=f'輸入你想篩選的縣市名稱、鄉鎮市區與村鄰里。 柯吳配{ko_wu:.4f}, 賴蕭配{lai_hsiao:.4f}, 侯趙配{hou_chao:.4f}')
-interface.launch()
+def update_town(county):
+    # 依縣市更新鄉鎮市區下拉選單
+    town = cosine_similarity_df[cosine_similarity_df['county'] == county]['town'].unique().tolist()
+    return gr.Dropdown(choices=town)
+
+def update_village(town):
+    # 依鄉鎮市區更新村鄰里下拉選單
+    village = cosine_similarity_df[cosine_similarity_df['town'] == town]['village'].unique().tolist()
+    return gr.Dropdown(choices=village)
+
+with gr.Blocks() as demo:
+    
+    theme='John6666/YntecDark'
+
+    gr.Markdown('# 找出章魚里\n'
+                f'全國得票率：柯吳配{ko_wu:.2f}, 賴蕭配{lai_hsiao:.2f}, 侯趙配{hou_chao:.2f}')
+    
+    with gr.Row():
+        with gr.Column():
+            gr.Markdown('# 餘弦相似度總表')
+            gr.DataFrame(cosine_similarity_df)
+        
+        with gr.Column():
+            
+            gr.Markdown(f'# 篩選縣市、鄉鎮市區與村鄰里以查看餘弦相似度')
+            county_dropdown = gr.Dropdown(
+                choices=cosine_similarity_df['county'].unique().tolist(),
+                label='選擇縣市',
+                interactive=True
+            )
+
+            town_dropdown = gr.Dropdown(
+                choices=cosine_similarity_df['town'].unique().tolist(),
+                label='選擇鄉鎮市區',
+                interactive=True
+            )
+
+            village_dropdown = gr.Dropdown(
+                choices=cosine_similarity_df['village'].unique().tolist(),
+                label='選擇村鄰里',
+                interactive=True
+            )
+            filtered_df = gr.DataFrame(cosine_similarity_df)
+    
+    county_dropdown.change(
+        fn=update_town,
+        inputs=county_dropdown,
+        outputs=town_dropdown
+        )
+    
+    town_dropdown.change(
+        fn=update_village,
+        inputs=town_dropdown,
+        outputs=village_dropdown
+        )
+    
+    village_dropdown.change(
+        fn=election.filter_county_town_village,
+        inputs=[county_dropdown, town_dropdown, village_dropdown],
+        outputs=filtered_df
+        )
+
+demo.launch()
+
+# interface= gr.Interface(
+#         fn=filter_county_town_village,
+#         inputs=[gr.DataFrame(cosine_similarity_df),'text', 'text', 'text'],
+#         outputs='dataframe', title='找出章魚里',
+#         description=f'輸入你想篩選的縣市名稱、鄉鎮市區與村鄰里。 柯吳配{ko_wu:.4f}, 賴蕭配{lai_hsiao:.4f}, 侯趙配{hou_chao:.4f}')
+# interface.launch()
--- proof_of_concept.py
+++ proof_of_concept.py
@@ -2,50 +2,58 @@ import sqlite3
 import numpy as np
 import pandas as pd
 
-def calculate_cosine_similarity():
-    connection = sqlite3.connect('data/taiwan_presidential_election_2024.db')
-    votes_by_village = pd.read_sql('SELECT * FROM votes_by_village;',con=connection)
-    connection.close()
+class TaiwanPresidentialElection2024:
+    def __init__(self):
+        self.vector_a, self.cosine_similarity_df = self.calculate_cosine_similarity()
 
-    # 全國得票率
-    total_votes = votes_by_village['sum_votes'].sum()
-    country_percentage = votes_by_village.groupby('number')['sum_votes'].sum() / total_votes
-    vector_a = country_percentage.values
+    def calculate_cosine_similarity(self):
+        connection = sqlite3.connect('data/taiwan_presidential_election_2024.db')
+        votes_by_village = pd.read_sql('SELECT * FROM votes_by_village;',con=connection)
+        connection.close()
 
-    # 村鄰里得票率
-    groupby_variables = ['county', 'town', 'village']
-    village_total_votes = votes_by_village.groupby(groupby_variables)['sum_votes'].sum().reset_index()
-    merged = pd.merge(votes_by_village, village_total_votes, on=groupby_variables, how='left')
-    merged['village_percentage'] = merged['sum_votes_x'] / merged['sum_votes_y']
-    merged = merged[['county', 'town', 'village', 'number', 'village_percentage']]
+        # 三組候選人全國得票率(向量a)
+        total_votes = votes_by_village['sum_votes'].sum()
+        country_percentage = votes_by_village.groupby('number')['sum_votes'].sum() / total_votes
+        vector_a = country_percentage.values
 
-    pivot_df = merged.pivot(index=['county', 'town', 'village'], columns='number', values='village_percentage').reset_index()
-    pivot_df.rename_axis(None, axis=1,inplace=True)
+        # 三組候選人村鄰里得票率(向量bi)
+        groupby_variables = ['county', 'town', 'village']
+        village_total_votes = votes_by_village.groupby(groupby_variables)['sum_votes'].sum().reset_index()
+        merged = pd.merge(votes_by_village, village_total_votes, on=groupby_variables, how='left')
+        merged['village_percentage'] = merged['sum_votes_x'] / merged['sum_votes_y']
+        merged = merged[['county', 'town', 'village', 'number', 'village_percentage']]
 
-    # 計算餘弦相似度
-    cosine_similarities = []
-    length_vector_a = pow((vector_a**2).sum(), .5)
-    for row in pivot_df.iterrows():
-        vector_bi = np.array([row[1][1], row[1][2], row[1][3]])
-        vector_a_dot_vector_bi = np.dot(vector_a, vector_bi)
-        length_vector_bi = pow((vector_bi**2).sum(), .5)
-        cosine_similarity = vector_a_dot_vector_bi / (length_vector_a * length_vector_bi)
-        cosine_similarities.append(cosine_similarity)
+        pivot_df = merged.pivot(index=['county', 'town', 'village'], columns='number', values='village_percentage').reset_index()
+        pivot_df.rename_axis(None, axis=1,inplace=True)
 
-    cosine_similarity_df = pivot_df.iloc[:,:]
-    cosine_similarity_df['cosine_similarity'] = cosine_similarities
-    cosine_similarity_df = cosine_similarity_df.sort_values(['cosine_similarity', 'county', 'town', 'village'],ascending=[False, True, True, True])
-    cosine_similarity_df = cosine_similarity_df.reset_index(drop=True).reset_index()
-    # 以index欄位當作rank
-    cosine_similarity_df['index'] = cosine_similarity_df['index'] + 1
-    column_names_to_revise ={'index': 'similarity_rank', 1:'candidate_1', 2: 'candidate_2', 3: 'candidate_3'}
-    cosine_similarity_df = cosine_similarity_df.rename(columns=column_names_to_revise)
-    return vector_a, cosine_similarity_df
+        # 計算餘弦相似度
+        cosine_similarities = []
+        length_vector_a = pow((vector_a**2).sum(), .5)
+        for row in pivot_df.iterrows():
+            vector_bi = np.array([row[1][1], row[1][2], row[1][3]])
+            vector_a_dot_vector_bi = np.dot(vector_a, vector_bi)
+            length_vector_bi = pow((vector_bi**2).sum(), .5)
+            cosine_similarity = vector_a_dot_vector_bi / (length_vector_a * length_vector_bi)
+            cosine_similarities.append(cosine_similarity)
 
-def filter_county_town_village(df, county_name: str, town_name: str, village_name: str):
-    county_condition = df['county'] == county_name
-    town_condition = df['town'] == town_name
-    village_condition = df['village'] == village_name
-    return df[county_condition & town_condition & village_condition]
+        cosine_similarity_df = pivot_df.iloc[:,:]
+        cosine_similarity_df['cosine_similarity'] = cosine_similarities
+        cosine_similarity_df = cosine_similarity_df.sort_values(['cosine_similarity', 'county', 'town', 'village'],ascending=[False, True, True, True])
+        cosine_similarity_df = cosine_similarity_df.reset_index(drop=True).reset_index()
+        
+        # 以index欄位當作rank
+        cosine_similarity_df['index'] = cosine_similarity_df['index'] + 1
+        column_names_to_revise ={'index': 'similarity_rank', 1:'candidate_1', 2: 'candidate_2', 3: 'candidate_3'}
+        cosine_similarity_df = cosine_similarity_df.rename(columns=column_names_to_revise)
+        return vector_a, cosine_similarity_df
 
-cosine_similarity_tuple = calculate_cosine_similarity()
+    def filter_county_town_village(self,county_name: str, town_name: str, village_name: str):
+        mask = (self.cosine_similarity_df['county'] == county_name) & \
+                (self.cosine_similarity_df['town'] == town_name) & \
+                (self.cosine_similarity_df['village'] == village_name)
+        filtered_df = self.cosine_similarity_df[mask]
+        return filtered_df
+
+election = TaiwanPresidentialElection2024()
+cosine_similarity_tuple = election.calculate_cosine_similarity()
+# print(cosine_similarity_tuple)
